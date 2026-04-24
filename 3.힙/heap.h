94ff09f71c524b929e9b0cfca5bbc5f4ba3fd4be
Antler9000 <ghenakfn20@naver.com>
2026-04-24 17:07:35 +0900

    [heap] BST_while 병합용 커밋

--- 3.힙/heap.h
+++ 3.힙/heap.h
@@ -1,8 +1,8 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+#include "../0.°øÅë/debug.h"
 #include <iostream>
-
 using namespace std;
 
 class Heap
@@ -153,5 +153,51 @@ class MaxHeap : public Heap
 public :
 	MaxHeap() : Heap() {}
 };
+
+void Heap::ReorderByPromoting()
+{
+	int targetIndex = m_itemNum - 1;
+	int parentIndex = getParentIndex(targetIndex);
+
+	while ((targetIndex > 0) && IsNotOrdered(parentIndex, targetIndex))
+	{
+		Swap(m_pDatum[targetIndex], m_pDatum[parentIndex]);
+		targetIndex = parentIndex;
+		parentIndex = getParentIndex(targetIndex);
+	}
+}
+
+void Heap::ReorderByDemoting()
+{
+	int targetIndex = 0;
+	int leftChildIndex = getLeftChildIndex(targetIndex);
+	int rightChildIndex = getRightChildIndex(targetIndex);
+	int targetChildIndex;
+
+	while (leftChildIndex <= m_itemNum)
+	{
+		if (rightChildIndex <= m_itemNum)
+		{
+			if (IsLeftChildTarget(leftChildIndex, rightChildIndex)) targetChildIndex = leftChildIndex;
+			else targetChildIndex = rightChildIndex;
+		}
+		else
+		{
+			targetChildIndex = leftChildIndex;
+		}
+
+		if (IsNotOrdered(targetIndex, targetChildIndex))
+		{
+			Swap(m_pDatum[targetIndex], m_pDatum[targetChildIndex]);
+			targetIndex = targetChildIndex;
+			leftChildIndex = getLeftChildIndex(targetIndex);
+			rightChildIndex = getRightChildIndex(targetIndex);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
 
 #endif //HEAP_H
